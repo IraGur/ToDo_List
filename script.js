6fe423a93c2e7a52b08c08bf671a5e2bd7568193
Irina Gurbanova <ira_kurbanova@hotmail.com>
2023-10-11 01:11:03 +0200

    functionality modified

--- script.js
+++ script.js
@@ -1,27 +1,58 @@
-const addTask = () => {
-   const taskInput = document.getElementById("taskInput");
-   const task = taskInput.value;
+let tasks = localStorage.getItem("tasks")
+   ? JSON.parse(localStorage.getItem("tasks"))
+   : [];
 
-   if (!task) return;
-
-   const taskList = document.getElementById("taskList");
-
-   const newTask = document.createElement("div");
-   newTask.classList.add("task");
-
-   const taskText = document.createElement("div");
-   taskText.classList.add("task-text");
-   taskText.innerHTML = task;
-
-   const deleteButton = document.createElement("button");
-   deleteButton.innerHTML = "Delete";
-   deleteButton.onclick = () => {
-      taskList.removeChild(newTask);
-   };
-
-   newTask.appendChild(taskText);
-   newTask.appendChild(deleteButton);
-   taskList.appendChild(newTask);
-
-   taskInput.value = "";
+const setWarningDisplayValue = (value) => {
+   const warning = document.querySelector(".warning");
+   warning.style.display = value;
 };
+
+const refreshTasks = () => {
+   const tasksContainer = document.getElementById("tasks-list");
+   tasksContainer.innerHTML = "";
+   if (tasks.length > 0) {
+      tasks.forEach((task) => {
+         const taskElement = document.createElement("div");
+         const removeButton = document.createElement("button");
+
+         removeButton.innerText = "X";
+         removeButton.className = "remove-button";
+         removeButton.dataset.taskId = task.id;
+         removeButton.addEventListener("click", removeTask);
+
+         taskElement.className = "task";
+         taskElement.innerText = task.value;
+         taskElement.appendChild(removeButton);
+         tasksContainer.appendChild(taskElement);
+      });
+   }
+};
+
+const addTask = () => {
+   const input = document.getElementById("task-input");
+   setWarningDisplayValue("none");
+
+   if (input.value.trim()) {
+      const taskItem = {};
+      const ids = tasks.map((task) => task.id);
+
+      taskItem.id = ids.length > 0 ? ids[ids.length - 1] + 1 : 1;
+      taskItem.value = input.value;
+      input.value = "";
+
+      tasks.push(taskItem);
+      localStorage.setItem("tasks", JSON.stringify(tasks));
+      refreshTasks();
+   } else {
+      setWarningDisplayValue("block");
+   }
+};
+
+const removeTask = (event) => {
+   const taskId = event.target.dataset.taskId;
+   tasks = tasks.filter((task) => task.id !== parseInt(taskId));
+   localStorage.setItem("tasks", JSON.stringify(tasks));
+   refreshTasks();
+};
+
+refreshTasks();
